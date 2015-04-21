53a0c1ae1310a466e10a9cf774d43255fbd02e41
sdeclere <sdeclere@gmail.com>
2015-04-21 15:18:56 +0200

    Add licence headers

--- gc.R
+++ gc.R
@@ -1,7 +1,20 @@
 ##
-## Compute GC curves. 
-##
+#Copyright (C) DECLERE 2015
+#This program is free software; you can redistribute it and/or
+#modify it under the terms of the GNU General Public License
+#as published by the Free Software Foundation; either version 2
+#of the License, or (at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+#Compute GC curves.
 library(seqinr)
 chroms = read.fasta(file = "CAGL.faa")
 chr = c("Cagl0A", "Cagl0B", "Cagl0C", "Cagl0D", "Cagl0E", "Cagl0F", "Cagl0G", "Cagl0H", "Cagl0I", "Cagl0J", "Cagl0K", "Cagl0L", "Cagl0M")
